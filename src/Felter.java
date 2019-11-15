7bc2bf9185b27e60d1a8ba26f196d8c3ce38c6ff
Christian Pone <ChristiaNotarmaso@gmail.com>
2019-11-15 16:43:07 +0100

    Tilføjet felter

--- src/Felter.java
+++ src/Felter.java
@@ -1,2 +1,72 @@
 public class Felter {
+
+    public void Felter(){
+        int x;
+        switch (x) {
+            char farve;
+            int pris;
+            String navn;
+            // HVID = 'A', BRUN = 'B', LYSEBLÅ = C, PINK = D, ORANGE = E, RØD = F, GUL = G, GRØN= H, BLÅ = I
+
+            case 0:
+                farve = 'A'; pris = 0; navn = "Start";
+            case 1:
+                farve = 'B'; pris = 1; navn = "Burger Baren";
+            case 2:
+                farve = 'B'; pris = 1; navn = "Pizzariaet";
+            case 3:
+                farve = 'A'; pris = 0; navn = "CHANCE";
+            case 4:
+                farve = 'C'; pris = 1; navn = "Slikbutikken";
+            case 5:
+                farve = 'C'; pris = 1; navn = "Iskiosken";
+            case 6:
+                farve = 'A'; pris = 0; navn = "PÅ BESØG I FÆNGSEL";
+            case 7:
+                farve = 'D'; pris = 2; navn = "Museet";
+            case 8:
+                farve = 'D'; pris = 2; navn = "Biblioteket";
+
+            case 9:
+                farve = 'A'; pris = 0; navn = "CHANCE";
+            case 10:
+                farve = 'E'; pris = 2; navn = "Skaterparken";
+            case 11:
+                farve = 'E'; pris = 2; navn = "Swimmingpoolen";
+            case 12:
+                farve = 'A'; pris = 0; navn = "GRATIS PARKERING";
+            case 13:
+                farve = 'F'; pris = 3; navn = "Spillehallen";
+            case 14:
+                farve = 'F'; pris = 3; navn = "Biografen";
+            case 15:
+                farve = 'A'; pris = 0; navn = "CHANCE";
+            case 16:
+                farve = 'G'; pris = 3; navn = "Legetøjsbutikken";
+            case 17:
+                farve = 'G'; pris = 3; navn = "Dyrehandlen";
+            case 18:
+                farve = 'A'; pris = 0; navn = "GÅ I FÆNGSEL";
+            case 19:
+                farve = 'H'; pris = 4; navn = "Bowlinghallen";
+            case 20:
+                farve = 'H'; pris = 4; navn = "Zoo";
+            case 21:
+                farve = 'A'; pris = 0; navn = "CHANCE";
+            case 22:
+                farve = 'I'; pris = 5; navn = "Vandlandet";
+            case 23:
+                farve = 'I'; pris = 5; navn = "Standpromenaden";
+
+
+
+
+
+
+
+
+        }
+    }
+
+
 }
