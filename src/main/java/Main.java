4baefaada302f82c5dbc04af13f87427328fd9a8
Folkmann <theme1256@gmail.com>
2019-11-18 15:21:54 +0100

    Spillet vil ikke gå igang før man har skrevet et korrekt antal spillere

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,25 +3,31 @@ import java.util.Scanner;
 public class Main {
     private static int numberOfPlayers = 0;
     private static int startBalance = 0;
+    private static Player[] players;
 
     public static void main(String[] args) {
-        getNumberOfPlayers();
-        Player[] players = new Player[numberOfPlayers];
-
-        switch(numberOfPlayers) {
-            case 2:
-                startBalance = 20;
-                break;
-            case 3:
-                startBalance = 18;
-                break;
-            case 4:
-                startBalance = 16;
-                break;
-            default:
-                System.out.println("Det var ikke et understøttet antal spillere.");
-                System.exit(0);
+        // Hent antallet af spillere og sæt start-balancen ud fra det
+        // Sikrer også at der kun kan spilles med et godkendt antal spillere
+        while (startBalance == 0) {
+            getNumberOfPlayers();
+            players = new Player[numberOfPlayers];
+            switch (numberOfPlayers) {
+                case 2:
+                    startBalance = 20;
+                    break;
+                case 3:
+                    startBalance = 18;
+                    break;
+                case 4:
+                    startBalance = 16;
+                    break;
+                default:
+                    System.out.println("Det var ikke et understøttet antal spillere.");
+                    break;
+            }
         }
+
+        // Opret spillerne
         for (int i = 0; i < numberOfPlayers; i++) {
             System.out.println("Spiller indtast dit navn");
             players[i] = new Player(startBalance);
@@ -31,7 +37,7 @@ public class Main {
         Shaker shaker = new Shaker();
 
         ChanceDeck chanceDeck = new ChanceDeck();
-        chanceDeck.blandkort();
+        chanceDeck.shuffle();
 
         Scanner scan = new Scanner(System.in);
 
@@ -65,7 +71,7 @@ public class Main {
     private static void chancekort(Player player, ChanceDeck chanceDeck) {
         if (player.currentFelt == 4 || player.currentFelt == 10 || player.currentFelt == 16 || player.currentFelt == 22) {
             System.out.println("____________________ \n CHANCEKORT \n____________________ \n");
-            System.out.println(chanceDeck.traekkort().toString());
+            System.out.println(chanceDeck.draw().toString());
             System.out.println("");
         }
     }
