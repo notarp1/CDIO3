45aade2811f2a11c6801a7d2ed4595e1f9de8a84
Christian Pone <ChristiaNotarmaso@gmail.com>
2019-11-27 15:25:15 +0100

    ændre navne

--- src/main/java/Model/Brik.java
+++ src/main/java/Model/Brik.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Brik {
 
 
-    private boolean skibLock = false;
-    private boolean hundLock = false;
-    private boolean katLock = false;
     private boolean bilLock = false;
+    private boolean racerbilLock = false;
+    private boolean traktorLock = false;
+    private boolean ufoLock = false;
 
 
     void brikSelect() {
-        System.out.println("Tryk 1 for Skibet \nTryk 2 for at være Hunden \nTryk 3 for at være Katten \nTryk 4 for at være Bilen");
+        System.out.println("Tryk 1 for at være Bilen \nTryk 2 for at være Racerbilen \nTryk 3 for at være Traktoren \nTryk 4 for at være UFO'en");
 
         while (true) {
             System.out.println("Vælg din brik");
@@ -21,46 +21,6 @@ public class Brik {
 
 
             if (characterSelect.equals("1")) {
-                if (!skibLock) {
-                    this.skibLock = true;
-                    System.out.println("Du har valgt skibet");
-
-                    break;
-                }
-            }
-            if (skibLock && characterSelect.equals("1")) {
-                System.out.println("Skibet er allerede taget");
-                continue;
-            }
-
-            if (characterSelect.equals("2")) {
-                if (!hundLock) {
-                    this.hundLock = true;
-                    System.out.println("Du har valgt Hunden");
-                   break;
-
-                }
-            }
-            if (hundLock && characterSelect.equals("2")) {
-                System.out.println("Hunden er allerede taget");
-
-                continue;
-            }
-
-            if (characterSelect.equals("3")) {
-                if (!katLock) {
-                    this.katLock = true;
-                    System.out.println("Du har valgt Katten");
-
-                    break;
-                }
-            }
-            if (katLock && characterSelect.equals("3")) {
-                System.out.println("Katten er allerede taget");
-                continue;
-            }
-
-            if (characterSelect.equals("4")) {
                 if (!bilLock) {
                     this.bilLock = true;
                     System.out.println("Du har valgt Bilen");
@@ -68,8 +28,48 @@ public class Brik {
                     break;
                 }
             }
-            if (bilLock && characterSelect.equals("4")) {
-                    System.out.println("Bilen er allerede taget");
+            if (bilLock && characterSelect.equals("1")) {
+                System.out.println("Bilen er allerede taget");
+                continue;
+            }
+
+            if (characterSelect.equals("2")) {
+                if (!racerbilLock) {
+                    this.racerbilLock = true;
+                    System.out.println("Du har valgt Racerbilen");
+                   break;
+
+                }
+            }
+            if (racerbilLock && characterSelect.equals("2")) {
+                System.out.println("Racerbilen er allerede taget");
+
+                continue;
+            }
+
+            if (characterSelect.equals("3")) {
+                if (!traktorLock) {
+                    this.traktorLock = true;
+                    System.out.println("Du har valgt Traktoren");
+
+                    break;
+                }
+            }
+            if (traktorLock && characterSelect.equals("3")) {
+                System.out.println("Traktoren er allerede taget");
+                continue;
+            }
+
+            if (characterSelect.equals("4")) {
+                if (!ufoLock) {
+                    this.ufoLock = true;
+                    System.out.println("Du har valgt UFO'en");
+
+                    break;
+                }
+            }
+            if (ufoLock && characterSelect.equals("4")) {
+                    System.out.println("UFO'en er allerede taget");
                 }
             }
         }
